b72c099d8d32d68cd678824d46b850033bd19712
permas <permasteo@gmail.com>
2021-08-11 17:35:24 +0800

    feat: implement pydantic validation for User

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator
 
 app = FastAPI()
 
-class Item(BaseModel):
+class User(BaseModel):
+    id: str
+    login: str
     name: str
-    price: float
-    is_offer: Optional[bool] = None
+    salary: float
+
+    @validator('salary')
+    def salary_positive(cls, v):
+        assert v >= 0, 'must be a positive number'
+        return v
+
+# try:
+#     User(
+#         id='samuel',
+#         login='scolvin',
+#         name=12,
+#         salary="-111.0",
+        
+#     )
+# except ValidationError as e:
+#     print(e)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/users/{user_id}")
+def read_user(user_id: int, q: Optional[str] = None):
+    return {"user_id": user_id, "q": q}
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User):
+    return {"user_name": user.name, "user_id": user_id}
