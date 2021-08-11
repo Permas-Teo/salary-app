4008951735e1d88150e4ecb9248ae6dd07d02db1
permas <permasteo@gmail.com>
2021-08-11 23:21:29 +0800

    feat: filter and validate csv data

--- sql_app/main.py
+++ sql_app/main.py
@@ -24,6 +24,46 @@ def get_db():
     finally:
         db.close()
 
+# Utility
+def filterDf(df):
+    return df[~df['id'].str.contains("#")] 
+
+# id and login must be unique. They cannot be repeated in another row 
+# => Assume invalid if ANY id or login is found duplicate in csv
+def validateCSV(df):
+    noDuplicatesId = {}
+    noDuplicatesLogin = {}
+    for index, row in df.iterrows():
+        currId = row["id"]
+        currLogin = row["login"]
+        currName = row["name"]
+        currSalary = row["salary"]
+
+        # Check all values not NAN
+        if (pd.isna(currId) or pd.isna(currLogin) or pd.isna(currName) or pd.isna(currSalary)):
+            return False
+
+        # Check salary is numeric and >= 0
+        if type(currSalary) == int or type(currSalary) == float:
+            if currSalary < 0:
+                return False
+        else:
+            return False
+        
+        # Check duplicates for id
+        if currId not in noDuplicatesId:
+            noDuplicatesId[currId] = True
+        else:
+            return False
+        
+        # Check duplicates for login
+        if currLogin not in noDuplicatesLogin:
+            noDuplicatesLogin[currLogin] = True
+        else:
+            return False
+
+    return True
+
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
@@ -47,7 +87,13 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 @app.post("/users/upload")
-async def create_upload_file(file: UploadFile = File(...)):
+def create_upload_file(file: UploadFile = File(...)):
     df = pd.read_csv(StringIO(str(file.file.read(), 'utf-8')), encoding='utf-8')
-    print(df)
+    
+    df = filterDf(df)
+    if (not validateCSV(df)):
+        raise HTTPException(status_code=422, detail="Data validation failed")
+
+    # updateDb(df)
+    
     return {"filename": file.filename}
