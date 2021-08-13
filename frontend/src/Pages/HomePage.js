e5ce655d4df97242f3d4e7635f4a41d197c28364
permas <permasteo@gmail.com>
2021-08-13 23:09:23 +0800

    refactor: bring Alerts up from FileUpload to HomePage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import { API_URL } from '../utils/constants';
+
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Box,
+  CloseButton,
+  Container,
+  Flex,
+} from '@chakra-ui/react';
+
 import { SalaryTable } from '../components/dashboard/salaryTable';
 import { FileUpload } from '../components/dashboard/fileUpload';
 
 const HomePage = () => {
   const [res, setRes] = useState('');
+  const [status, setStatus] = useState('');
 
   useEffect(() => {
     fetch(API_URL + '/users')
@@ -16,9 +29,50 @@ const HomePage = () => {
       });
   }, [res]);
 
+  function handleStatusChange(status) {
+    setStatus(status);
+  }
+
   return (
     <Layout>
-      <FileUpload />
+      {status && status === 'Success' && (
+        <Alert variant="solid" status="success">
+          <AlertIcon />
+          <AlertTitle mr={2}>File uploaded succesfully!</AlertTitle>
+          <CloseButton
+            position="absolute"
+            right="8px"
+            top="8px"
+            onClick={() => setStatus('')}
+          />
+        </Alert>
+      )}
+
+      {status && status !== 'Success' && (
+        <Alert variant="solid" status="error">
+          <AlertIcon />
+          <AlertTitle mr={2}>Error!</AlertTitle>
+          <AlertDescription>{status}</AlertDescription>
+          <CloseButton
+            position="absolute"
+            right="8px"
+            top="8px"
+            onClick={() => setStatus('')}
+          />
+        </Alert>
+      )}
+
+      <Container maxWidth={'10xl'}>
+        <Flex flexWrap={'wrap'} justify="center">
+          <Box m={2}>
+            <FileUpload onStatusChange={handleStatusChange} flex="1" />
+          </Box>
+          <Box m={2}>
+            <FileUpload flex="1" />
+          </Box>
+        </Flex>
+      </Container>
+
       {res ? <SalaryTable data={res} /> : <></>}
     </Layout>
   );
--- frontend/src/components/dashboard/fileUpload.js
+++ frontend/src/components/dashboard/fileUpload.js
@@ -1,12 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
   Box,
   Button,
-  CloseButton,
   Container,
   Heading,
   List,
@@ -24,7 +19,7 @@ import {
 } from '../../styles/dropzone-styles';
 import { CheckIcon } from '@chakra-ui/icons';
 
-export const FileUpload = () => {
+export const FileUpload = ({ onStatusChange }) => {
   const [filesToSend, setFilesToSend] = useState([]);
   const [status, setStatus] = useState('');
 
@@ -62,78 +57,50 @@ export const FileUpload = () => {
   );
 
   return (
-    <>
-      {status && status === 'Success' && (
-        <Alert variant="solid" status="success">
-          <AlertIcon />
-          <AlertTitle mr={2}>File uploaded succesfully!</AlertTitle>
-          <CloseButton
-            position="absolute"
-            right="8px"
-            top="8px"
-            onClick={() => setStatus('')}
-          />
-        </Alert>
-      )}
-
-      {status && status !== 'Success' && (
-        <Alert variant="solid" status="error">
-          <AlertIcon />
-          <AlertTitle mr={2}>Error!</AlertTitle>
-          <AlertDescription>{status}</AlertDescription>
-          <CloseButton
-            position="absolute"
-            right="8px"
-            top="8px"
-            onClick={() => setStatus('')}
-          />
-        </Alert>
-      )}
-
-      <Container
-        maxW={'md'}
-        bg={'whiteAlpha.100'}
-        boxShadow={'xl'}
-        rounded={'lg'}
-        p={6}
-        my={2}
-        direction={'column'}
+    <Container
+      maxW={'md'}
+      minW={'sm'}
+      bg={'whiteAlpha.100'}
+      boxShadow={'xl'}
+      rounded={'lg'}
+      p={6}
+      direction={'column'}
+    >
+      <Heading
+        as={'h2'}
+        fontSize={{ base: 'xl', sm: '2xl' }}
+        textAlign={'center'}
+        my={5}
       >
-        <Heading
-          as={'h2'}
-          fontSize={{ base: 'xl', sm: '2xl' }}
-          textAlign={'center'}
-          my={5}
+        CSV Upload
+      </Heading>
+      <Box {...getRootProps({ style })} m={2}>
+        <input {...getInputProps()} />
+        <Text>Drag and drop / Click here to upload csv file</Text>
+      </Box>
+      <VStack>
+        <List py={2}>{files}</List>
+        <Button
+          onClick={() => {
+            const fileToSend = acceptedFiles[0];
+            const formData = new FormData();
+            formData.append(`file`, fileToSend);
+            fetch(API_URL + '/users/upload', {
+              method: 'POST',
+              body: formData,
+            })
+              .then(response => response.json())
+              .then(res => {
+                console.log(res.detail);
+                onStatusChange(res.detail);
+                setStatus(res.detail);
+              });
+            setFilesToSend([]);
+          }}
         >
-          CSV Upload
-        </Heading>
-        <Box {...getRootProps({ style })} m={2}>
-          <input {...getInputProps()} />
-          <Text>Drag and drop / Click here to upload csv file</Text>
-        </Box>
-        <VStack>
-          <List py={2}>{files}</List>
-          <Button
-            onClick={() => {
-              const fileToSend = acceptedFiles[0];
-              const formData = new FormData();
-              formData.append(`file`, fileToSend);
-              fetch(API_URL + '/users/upload', {
-                method: 'POST',
-                body: formData,
-              })
-                .then(response => response.json())
-                .then(res => {
-                  console.log(res.detail);
-                  setStatus(res.detail);
-                });
-              setFilesToSend([]);
-            }}
-          >
-            Upload
-          </Button>
-        </VStack>
-      </Container>
-    </>
+          Upload
+        </Button>
+      </VStack>
+    </Container>
   );
 };
