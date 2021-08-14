d2a3bcf35ccb265473ac33d0cbd6655df70c10a3
permas <permasteo@gmail.com>
2021-08-14 20:59:31 +0800

    refactor: extract query filter component from HomePage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,21 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
-import { Box, Container, Flex, Center } from '@chakra-ui/react';
-
-import {
-  Button,
-  FormControl,
-  Heading,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-  Stack,
-} from '@chakra-ui/react';
-
+import { Box, Container, Flex } from '@chakra-ui/react';
 import { SalaryTable } from '../components/dashboard/salaryTable';
 import { FileUpload } from '../components/dashboard/fileUpload';
+import { QueryFilter } from '../components/dashboard/queryFilter';
 import { Alerts } from '../components/dashboard/alerts';
 import { fetchUsers } from '../api/api';
 
@@ -75,94 +63,15 @@ const HomePage = () => {
             />
           </Box>
           <Box m={2}>
-            <Container
-              height={'270px'}
-              maxW={'xs'}
-              minW={'xs'}
-              bg={'whiteAlpha.100'}
-              boxShadow={'xl'}
-              rounded={'lg'}
-              p={6}
-              direction={'column'}
-            >
-              <Heading
-                as={'h2'}
-                fontSize={{ base: 'xl', sm: '2xl' }}
-                textAlign={'center'}
-                my={5}
-              >
-                Filter
-              </Heading>
-
-              <Stack
-                direction={'column'}
-                as={'form'}
-                spacing={'12px'}
-                onSubmit={e => {
-                  e.preventDefault();
-                  let resultData = fetchUsers(sortToggle, minSalary, maxSalary);
-                  resultData.then(resultData => {
-                    setRes(resultData.results);
-                    setPage(0);
-                    setTotalPages(resultData.totalPages);
-                  });
-                }}
-              >
-                <FormControl>
-                  <NumberInput
-                    size="md"
-                    min={0}
-                    step={100}
-                    onChange={e => {
-                      setMinSalary(e);
-                    }}
-                    value={minSalary}
-                  >
-                    <NumberInputField
-                      borderColor={'gray.300'}
-                      placeholder={'Min Salary'}
-                    />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                </FormControl>
-                <FormControl>
-                  <NumberInput
-                    size="md"
-                    min={minSalary ? minSalary : 0}
-                    step={100}
-                    onChange={e => {
-                      setMaxSalary(e);
-                    }}
-                    value={maxSalary}
-                  >
-                    <NumberInputField
-                      borderColor={'gray.300'}
-                      placeholder={'Max Salary'}
-                    />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                </FormControl>
-                <Center>
-                  <Button
-                    m={2}
-                    onClick={() => {
-                      reset();
-                    }}
-                  >
-                    {'Reset'}
-                  </Button>
-                  <Button colorScheme={'blue'} m={2} type="submit">
-                    {'Calculate'}
-                  </Button>
-                </Center>
-              </Stack>
-            </Container>
+            <QueryFilter
+              setRequestUpdate={setRequestUpdate}
+              setPage={setPage}
+              minSalary={minSalary}
+              setMinSalary={setMinSalary}
+              maxSalary={maxSalary}
+              setMaxSalary={setMaxSalary}
+              reset={reset}
+            />
           </Box>
         </Flex>
       </Container>
